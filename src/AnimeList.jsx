5de4232a1cf73a78eb6ad9407d4f00126bbe8d39
gdslhack <34924797+gdslhack@users.noreply.github.com>
2024-08-04 16:55:47 +0700

    Update AnimeList.jsx

--- src/AnimeList.jsx
+++ src/AnimeList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import SearchBar from './SearchBar';
+import SearchBar from './components/SearchBar';
 
 const AnimeList = () => {
     const [animeList, setAnimeList] = useState([]);

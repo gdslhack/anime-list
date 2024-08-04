2a0c48fbe7a6b2cf2a5eda1102a19dc307eac65a
gdslhack <34924797+gdslhack@users.noreply.github.com>
2024-08-04 16:08:18 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AnimeList from './AnimeList';
+
+function App() {
+    return (
+        <div className="App">
+            <AnimeList />
+        </div>
+    );
+}
+
+export default App;
